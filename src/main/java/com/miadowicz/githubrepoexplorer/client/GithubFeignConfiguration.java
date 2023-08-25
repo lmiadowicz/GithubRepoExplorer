7021f98083328fb10dd12c74f255743e29c40d92
lukaszmiadowicz <l.miadowicz@gmail.com>
2023-08-25 16:04:47 +0200

    Changed docker image

--- src/main/java/com/miadowicz/githubrepoexplorer/client/GithubFeignConfiguration.java
+++ src/main/java/com/miadowicz/githubrepoexplorer/client/GithubFeignConfiguration.java
@@ -6,6 +6,7 @@ import feign.RequestInterceptor;
 import feign.Response;
 import feign.codec.Decoder;
 import feign.jackson.JacksonDecoder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,18 +14,18 @@ import java.io.IOException;
 
 @Configuration
 public class GithubFeignConfiguration {
-    private static final int CONNECT_TIMEOUT_MILLIS = 10000;
-    private static final int READ_TIMEOUT_MILLIS = 10000;
+    private static final int CONNECT_TIMEOUT_MILLIS = 1000;
+    private static final int READ_TIMEOUT_MILLIS = 1000;
     @Bean
     public Decoder decoder() {
         return new JacksonDecoder();
     }
 
     @Bean
-    public RequestInterceptor requestInterceptor() {
+    public RequestInterceptor requestInterceptor(/*@Value("${GITHUB_API_TOKEN}") String githubApiToken*/) {
         return template -> {
-            String githubToken = "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";
-            template.header("Authorization", "token %s".formatted(githubToken));
+          String githubApiToken = "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";
+            template.header("Authorization", "token %s".formatted(githubApiToken));
         };
     }
     @Bean
