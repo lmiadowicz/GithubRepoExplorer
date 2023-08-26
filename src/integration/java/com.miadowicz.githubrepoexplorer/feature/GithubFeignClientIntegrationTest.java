5399731cbd17801b29a5470b816eb5f75620cdec
lukaszmiadowicz <l.miadowicz@gmail.com>
2023-08-26 08:40:26 +0200

    Integration tests
    * Added classes annotation to SpringBootTest pointing to GithubFeignClientIntegrationTest.class

--- src/integration/java/com.miadowicz.githubrepoexplorer/feature/GithubFeignClientIntegrationTest.java
+++ src/integration/java/com.miadowicz.githubrepoexplorer/feature/GithubFeignClientIntegrationTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(classes = GithubFeignClientIntegrationTest.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @ExtendWith(WireMockExtension.class)
